f370ed4fc748a19e4f5428b5c600c8e690e8dc68
Art Scheel <ascheel@gmail.com>
2025-04-17 19:59:49 -0600

    big kickstart changes

--- kickstart.sh
+++ kickstart.sh
@@ -3,54 +3,50 @@
 set -e
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
+CONFIGFILE="${SCRIPT_DIR}/kickstart.yml"
 
 pushd () {
-    # This exists just so we can suppress stdout
-    command pushd "$@" >/dev/null 2>&1
+	# This exists just so we can suppress stdout
+	command pushd "$@" >/dev/null 2>&1
 }
 
 popd () {
-    # This exists just so we can suppress stdout
-    command popd "$@" >/dev/null 2>&1
+	# This exists just so we can suppress stdout
+	command popd "$@" >/dev/null 2>&1
 }
 
 pushd ~
 
 if [[ "$(python3 "${SCRIPT_DIR}/scripts/getos.py" --distro)" == "ubuntu" ]]
 then
-    # Ubuntu only
-    sudo apt update
-    sudo apt upgrade -y
+	# Ubuntu only
+	# sudo apt update
+	sudo apt upgrade -y
 
-    # Install Python3
-    if ! command -v python3 > /dev/null 2>&1
-    then
-        echo "Python3 not installed.  Installing."
-        sudo apt install -y \
-            python3 \
-            python3-venv \
-            build-essential \
-            git
-    else
-        echo "Python3 already installed."
-    fi
-    # End Install Python3
+	# Install packages from apt
+	for package in $(yq -r '.apt.packages' "${CONFIGFILE}" | jq -r "@sh" | tr -d "'"); do
+		if ! dpkg -s $package >/dev/null 2>&1; then
+			echo "Installing $package"
+			sudo apt install -y $package
+		fi
+	done
 fi
 
-# Configure git
-fgrep "email =" ~/.gitconfig >/dev/null 2>&1 || git config --global user.email "ascheel@gmail.com"
-fgrep "name =" ~/.gitconfig >/dev/null 2>&1 || git config --global user.name "Art Scheel"
+# Create tmp directory
+export TMPDIR="${SCRIPT_DIR}"/tmp
+[[ ! -d "${TMPDIR}" ]] && mkdir "${TMPDIR}"
+# [[ ! -d "${SCRIPT_DIR}"/tmp ]] && mkdir "${SCRIPT_DIR}"/tmp
+
+# Install Golang
+source $SCRIPT_DIR/scripts/kickstart.golang.sh
+
+# .bashrc.d
+source $SCRIPT_DIR/scripts/kickstart.bashrcd.sh
 
 # Configure SSH
-[[ ! -d "~/.ssh" ]] && mkdir ~/.ssh
-chmod 700 ~/.ssh
+source $SCRIPT_DIR/scripts/kickstart.ssh.sh
 
-# Install Pip
-curl -s https://bootstrap.pypa.io/get-pip.py | python3
-
-python3 -m venv .venv
-python3 -m pip install -r "${SCRIPT_DIR}/requirements.txt"
-
-python3 "${SCRIPT_DIR}/scripts/kickstart.py"
+# Configure git
+source $SCRIPT_DIR/scripts/kickstart.git.sh
 
 popd
