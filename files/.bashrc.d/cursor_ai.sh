a5b59cdb4d8baacf8a4556528d60b0f330c97b40
scheel <scheel@adobe.com>
2026-06-25 10:56:24 -0600

    Set cursor.ai path to alias 'code'

--- files/.bashrc.d/cursor_ai.sh
+++ files/.bashrc.d/cursor_ai.sh
@@ -1,1 +1,1 @@
-export PATH="/mnt/c/Users/Art/AppData/Local/Programs/cursor/resources/app/bin:$PATH"
+[[ -n "$WSL_DISTRO_NAME" ]] && export PATH="/mnt/c/Users/Art/AppData/Local/Programs/cursor/resources/app/bin:$PATH"
